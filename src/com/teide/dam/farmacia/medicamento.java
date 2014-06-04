e5956e823ada15c5e76c0579adb17005d38a755b
Borja <Borja@Borja-TOSH.home>
2014-06-05 00:18:52 +0200

    prueba

--- src/com/teide/dam/farmacia/medicamento.java
+++ src/com/teide/dam/farmacia/medicamento.java
@@ -4,10 +4,22 @@
  */
 package com.teide.dam.farmacia;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Borja
  */
 public class medicamento {
     
+    private ArrayList<almacen> listado;
+    
+    public medicamento(){
+    listado = new ArrayList<>();
+    }
+    
+    public void crearMedicamento(String nombre, String fechaFab, String fechaCad, String tipo, String principioActivos, int precio){
+        medicamento m = new medicamento(nombre, fechaFab, fechaCad, tipo, principioActivos, precio);
+        
+    }
 }
