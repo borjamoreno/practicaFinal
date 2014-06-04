8b65307e66c10321c76d8d824076e68c61a30026
Borja <Borja@Borja-TOSH.home>
2014-06-04 23:58:35 +0200

    prueba

--- src/com/teide/dam/farmacia/almacen.java
+++ src/com/teide/dam/farmacia/almacen.java
@@ -11,9 +11,55 @@ package com.teide.dam.farmacia;
 public class almacen {
     String nombre, fechaFab, fechaCad, tipo, principioActivos;
     int precio;
+
     //FechaFab = Fabricacion, FechaCad = FechaCad, principiosActivos deben tener nombre y cantidad en mg.
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaFab() {
+        return fechaFab;
+    }
+
+    public void setFechaFab(String fechaFab) {
+        this.fechaFab = fechaFab;
+    }
+
+    public String getFechaCad() {
+        return fechaCad;
+    }
+
+    public void setFechaCad(String fechaCad) {
+        this.fechaCad = fechaCad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPrincipioActivos() {
+        return principioActivos;
+    }
+
+    public void setPrincipioActivos(String principioActivos) {
+        this.principioActivos = principioActivos;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
 }
 
-
-/*lo lees??
-si*/
